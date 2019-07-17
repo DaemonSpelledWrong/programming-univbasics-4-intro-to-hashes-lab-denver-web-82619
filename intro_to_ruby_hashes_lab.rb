93821773ffa269929f08bd98dc7d32fe5878f092
Damon Self <damongself@gmail.com>
2019-07-17 17:44:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,9 +26,13 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  return hash[key]
+  
 end
 
 def update_counting_hash(hash, key)
-  
+  if hash[key]
+    hash[key] += 1
+  else 
+    hash[key] = 1
+  end
 end
